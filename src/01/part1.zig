5c57955ecd53e9dfdd7aaba3d9abd6b3ea090064
sea gin <cijin@seagin.me>
2024-10-28 08:02:10 +0700

    Batman

---
+++ src/01/part1.zig
@@ -0,0 +1,5 @@
+const print = @import("std").debug.print;
+
+pub fn part_one() void {
+    print("welcome to part one, day one", .{});
+}
---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const partOne = @import("01/part1.zig");
+
+pub fn main() void {
+    partOne.part_one();
+}
