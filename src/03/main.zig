79a7dbe1a5841f3dcfa7bf66357e938a9084f7df
sea gin <cijin@seagin.me>
2024-11-05 07:09:55 +0700

    init day 3 part 1

--- src/03/main.zig
+++ src/03/main.zig
@@ -1,6 +1,25 @@
 const std = @import("std");
+const fs = std.fs;
 const print = std.debug.print;
 
 pub fn solve() !void {
-    print("attempting q3", .{});
+    const file = try fs.cwd().openFile("src/03/file.txt", .{});
+    defer file.close();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const deinitStatus = gpa.deinit();
+        if (deinitStatus == .leak) {
+            std.log.err("Memory leak", .{});
+        }
+    }
+
+    while (file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize)) catch |err| {
+        std.log.err("Failed to read line: {s}\n", .{@errorName(err)});
+        return;
+    }) |line| {
+        defer allocator.free(line);
+        print("{s}\n", .{line});
+    }
 }
